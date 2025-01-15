cdc3f581de67a6285998e5edfd0f04f0a4cf9103
Rohit Pidishetty <61237636+rohitpidishetty@users.noreply.github.com>
2025-01-15 19:18:20 +0530

    Update views.py

--- ittacademy/views.py
+++ ittacademy/views.py
@@ -9,13 +9,6 @@ import os
 
 API_KEY = 'sasejsckcqxg'
 API_SECRET = '29wa6nxaw5bycfk96q45trrbbaz6bazujdecwcgj8jfkbrkuxw7emavamur36erm'
-directory = os.path.dirname(os.path.abspath(__file__))
-SERVICE_ACCOUNT_PATH = os.path.join(directory, r'itt-academy-firebase-adminsdk-t7r0w-e7f7f8501d.json')
-
-cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://itt-academy-default-rtdb.firebaseio.com/' 
-})
 
 @csrf_exempt
 def create_user(request):
@@ -24,7 +17,6 @@ def create_user(request):
     name = mail[:mail.index('@')].strip()
     client = Stream(api_key=API_KEY, api_secret=API_SECRET, timeout=3.0)
     token = client.create_token(user_id=f"{name}-id")
-    ref = db.reference('/')
-    approval_status = ref.child('approval').get()
+    approval_status = True
     return JsonResponse({"user": name, "token": token, "user_id": f'{name}-id', "api": API_KEY, "approved": approval_status})    
   return JsonResponse({"status": 404})
