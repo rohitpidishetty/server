0662c30ac8ae2d71fd68108e4b3b058c05f8e980
Rohit Pidishetty <61237636+rohitpidishetty@users.noreply.github.com>
2025-01-19 21:16:08 +0530

    Update views.py

--- ittacademy/views.py
+++ ittacademy/views.py
@@ -16,32 +16,8 @@ def create_user(request):
         mail = request.GET.get('mail')
         if not mail:
             return JsonResponse({"error": "Email not provided."}, status=400)
-        
-        TYPE = os.getenv("TYPE")
-        PROJECT_ID = os.getenv("PROJECT_ID")
-        PRIVATE_KEY_ID = os.getenv("PRIVATE_KEY_ID")
-        PRIVATE_KEY = os.getenv("PRIVATE_KEY")
-        CLIENT_EMAIL = os.getenv("CLIENT_EMAIL")
-        CLIENT_ID = os.getenv("CLIENT_ID")
-        AUTH_URI = os.getenv("AUTH_URI")
-        TOKEN_URI = os.getenv("TOKEN_URI")
-        AUTH_PROVIDER_X509_CERT_URL = os.getenv("AUTH_PROVIDER_X509_CERT_URL")
-        CLIENT_X509_CERT_URL = os.getenv("CLIENT_X509_CERT_URL")
-        UNIVERSE_DOMAIN = os.getenv("UNIVERSE_DOMAIN")
 
-        service_acc_info = {
-            "type": TYPE,
-            "project_id": PROJECT_ID,
-            "private_key_id": PRIVATE_KEY_ID,
-            "private_key": PRIVATE_KEY,
-            "client_email": CLIENT_EMAIL,
-            "client_id": CLIENT_ID,
-            "auth_uri": AUTH_URI,
-            "token_uri": TOKEN_URI,
-            "auth_provider_x509_cert_url": AUTH_PROVIDER_X509_CERT_URL,
-            "client_x509_cert_url": CLIENT_X509_CERT_URL,
-            "universe_domain": UNIVERSE_DOMAIN
-        }
+        service_acc_info =  os.getenv("FIREBASE_PRIVATE_KEY")
 
         cred = credentials.Certificate(service_acc_info)
         firebase_admin.initialize_app(cred, {
