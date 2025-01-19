a4b3d88a226ab93a787115822a6d463d5487509d
Rohit Pidishetty <61237636+rohitpidishetty@users.noreply.github.com>
2025-01-19 20:35:52 +0530

    Update views.py

--- ittacademy/views.py
+++ ittacademy/views.py
@@ -2,6 +2,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from getstream import Stream
 import os
+import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase import db
 
 API_KEY = 'sasejsckcqxg'
 API_SECRET = '29wa6nxaw5bycfk96q45trrbbaz6bazujdecwcgj8jfkbrkuxw7emavamur36erm'
@@ -25,7 +29,25 @@ def create_user(request):
         CLIENT_X509_CERT_URL = os.getenv("CLIENT_X509_CERT_URL")
         UNIVERSE_DOMAIN = os.getenv("UNIVERSE_DOMAIN")
 
-        
+        service_acc_info = {
+            "type": TYPE,
+            "project_id": PROJECT_ID,
+            "private_key_id": PRIVATE_KEY_ID,
+            "private_key": PRIVATE_KEY,
+            "client_email": CLIENT_EMAIL,
+            "client_id": CLIENT_ID,
+            "auth_uri": AUTH_URI,
+            "token_uri": TOKEN_URI,
+            "auth_provider_x509_cert_url": AUTH_PROVIDER_X509_CERT_URL,
+            "client_x509_cert_url": CLIENT_X509_CERT_URL,
+            "universe_domain": UNIVERSE_DOMAIN
+        }
+
+        cred = credentials.Certificate(service_account_info)
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://itt-academy-default-rtdb.firebaseio.com/' 
+        })
+        ref = db.reference('/approval')
         
         name = mail[:mail.index('@')].strip()
         client = Stream(api_key=API_KEY, api_secret=API_SECRET, timeout=3.0)
@@ -38,7 +60,7 @@ def create_user(request):
             "user_id": f'{name}-id', 
             "api": API_KEY, 
             "approved": status,
-            "test": UNIVERSE_DOMAIN
+            "test": reg.get()
         })
         return response
 
