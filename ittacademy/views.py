b36c1a87d2d0b89388d0e15e8382a0d849affee1
Rohit Pidishetty <61237636+rohitpidishetty@users.noreply.github.com>
2025-01-19 22:49:41 +0530

    Update views.py

--- ittacademy/views.py
+++ ittacademy/views.py
@@ -17,12 +17,12 @@ def create_user(request):
         if not mail:
             return JsonResponse({"error": "Email not provided."}, status=400)
 
-        # cred = credentials.Certificate(FIREBASE)
-        # firebase_admin.initialize_app(cred, {
-        #     'databaseURL': 'https://itt-academy-default-rtdb.firebaseio.com/' 
-        # })
-        # ref = db.reference('/')
-        data = FIREBASE
+        cred = credentials.Certificate(FIREBASE)
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://itt-academy-default-rtdb.firebaseio.com/' 
+        })
+        ref = db.reference('/')
+        data = str(ref.get())
         name = mail[:mail.index('@')].strip()
         client = Stream(api_key=API_KEY, api_secret=API_SECRET, timeout=3.0)
         token = client.create_token(user_id=f"{name}-id")
