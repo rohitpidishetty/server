8a83d176007af939b03e0fec49547157cd02eb7b
Rohit Pidishetty <61237636+rohitpidishetty@users.noreply.github.com>
2025-02-02 21:49:30 -0600

    Update views.py

--- ittacademy/views.py
+++ ittacademy/views.py
@@ -5,6 +5,16 @@ import os
 import json
 import firebase_admin
 from firebase_admin import credentials, db
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_ADDRESS = os.getenv("EMAIL")
+EMAIL_PASSWORD = os.getenv("EMAIL_KEY")
 
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
@@ -55,9 +65,27 @@ def purchase(request):
             data = json.loads(request.body)
             details = data['payload']['details']
             name = details['name']
- 
-          
-            return JsonResponse({"details": name})
+            email = details['email']
+            phone = details['phone']
+            order = details['order']
+            product = order['product']
+            price = order['price']
+            previous_price = order['previous_price']
+            try:
+                message = f'NAME: {name}\nEMAIL: {email}\n PHONE: {phone}\nPRODUCT: {product}\nPRICE: {price}\nPREVIOUS PRICE: {previous_price}'
+                msg = MIMEMultipart()
+                msg["From"] = EMAIL_ADDRESS
+                msg["To"] = 'rohitpidishetty@gmail.com'
+                msg["Subject"] = f'Merchandise request by {name}'
+                msg.attach(MIMEText(message, "plain"))
+                server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+                server.starttls()  # Secure connection
+                server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)  # Login
+                server.sendmail(EMAIL_ADDRESS, to_email, msg.as_string())  # Send email
+                server.quit()
+                return JsonResponse({"status": 200})
+            except Exception as e:
+                return JsonResponse({"status": 400})
         except Exception as e:
-            return JsonResponse({"message": "Server broke"}, status=400)
+            return JsonResponse({"status": 400})
         
