3e883f868b0203fc3fc3268062449c65c27a8500
Rohit Pidishetty <61237636+rohitpidishetty@users.noreply.github.com>
2025-02-02 22:53:29 -0600

    Update views.py

--- ittacademy/views.py
+++ ittacademy/views.py
@@ -8,8 +8,6 @@ from firebase_admin import credentials, db
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
